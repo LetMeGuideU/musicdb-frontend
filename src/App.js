a17964cf15ace88696eeebbebab27da179669b9a
LetMeGuideU <53796803+LetMeGuideU@users.noreply.github.com>
2020-05-07 13:49:39 +0300

    deleted rest calls and Axios and added graphQL calls using apollo.Also i deleted the table component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import Nav from "./components/Nav";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import Table from "./components/Table";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 function App() {
+  const albums = gql`
+    {
+      allAlbums {
+        name
+      }
+    }
+  `;
   return (
     <>
       <Nav></Nav>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { ApolloClient, InMemoryCache, HttpLink, gql } from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: "http://127.0.0.1:8000/graphql",
+});
+
+const client = new ApolloClient({
+  link,
+  cache,
+});
+console.log(client);
+client
+  .query({
+    query: gql`
+      {
+        allAlbums {
+          name
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result.data));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
